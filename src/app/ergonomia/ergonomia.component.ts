a02d9d44e917c08fbaba0089be890e9fb27e21a7
apizon <aleksanderpizon@gmail.com>
2024-08-18 18:45:58 +0200

    implemented buttons

--- src/app/ergonomia/ergonomia.component.ts
+++ src/app/ergonomia/ergonomia.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { printPdf as printPdfUtil } from '../utils/print.util';
+import { htmlToText } from 'html-to-text';
+
 
 
 @Component({
@@ -12,7 +14,8 @@ export class ErgonomiaComponent {
   widok = 1
   radio1:  number ;  
   radio2:  number ;  
-  radio3:  number ;  
+  radio3:  number ;
+  clicked: Record<number, boolean> = {};
   clicked1: boolean = false;
   clicked2: boolean = false;
   clicked3: boolean = false;
@@ -21,6 +24,9 @@ export class ErgonomiaComponent {
   clicked6: boolean = false;
   clicked7: boolean = false;
   clicked8: boolean = false;
+  recommendations: Record<number, HTMLElement | string> = {};
+  recommendations1: string = '';
+  recommendations2: string = '';
   setClicked1()
   {
     this.clicked1 = true;
@@ -57,6 +63,33 @@ export class ErgonomiaComponent {
   {
     this.widok = _view;
   }
+
+
+  editRecommendations(id: number) {
+    const defaultTextElement = document.getElementById(`defaultText${id}`);
+    if (defaultTextElement && this.recommendations[id]) {
+      const htmlContent = defaultTextElement.innerHTML.trim();
+      const formattedText = htmlToText(htmlContent, {
+        wordwrap: 130,
+      });
+      this.recommendations[id] = formattedText; // Wypełnij textarea domyślnym tekstem
+    }
+    this.clicked[id] = true; // Pokaż textarea
+  }
+
+  insertDefaultRecommendations(id: number) {
+    const defaultTextElement = document.getElementById(`defaultText${id}`);
+    if (defaultTextElement) {
+      // Przypisz referencję na element HTML
+      // console.log(defaultTextElement)
+      const formattedText = defaultTextElement.innerHTML.trim();
+
+      this.recommendations[id] = formattedText;
+      this.clicked[id] = false;
+    }
+  }
+  
+  
 
   savePdf(id: number) {
     const pdfName = this.getPdfNameForSelection(id);
